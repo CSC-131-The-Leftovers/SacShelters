fe0fdd177b73213171b79a4d4db8e53deaa11cbf
Carter Lavering <carterlavering@gmail.com>
2024-04-27 21:44:49 -0700

    sign up page

--- pages/signUp.vue
+++ pages/signUp.vue
@@ -1,10 +1,44 @@
 <template>
-  <signUp/>
+  <form @submit.prevent="signUp">
+    <label>Email: </label>
+    <input type="email" required v-model = "email">
+
+    <label>Password: </label>
+    <input type="password" required v-model = "password">
+
+    <div class = "submit">
+    <button>Create an Account</button>
+  </div>
+
+  </form>
+  <p>email: {{ email }}</p>
+  <p>pass: {{ password }}</p>
+  <p>error: {{ errorMsg }}</p>
+  <p>success: {{ successMsg }}</p>
+
+  
 </template>
 
-<script setup>
- 
-import signUp from '~/components/signUpComponent.vue';
+<script setup lang="ts">
+
+const supabase = useSupabaseClient()
+const email = ref("")
+const password = ref(null)
+const errorMsg = ref(null)
+const successMsg = ref(null)
+
+async function signUp() {
+  try {
+    const { data, error } = await supabase.auth.signUp({
+      email: email.value,
+      password: password.value,
+    })
+    if (error) throw error;
+    successMsg.value = "Check your email to confirm your account."
+  } catch (error) {
+    errorMsg.value = error.message;
+  }
+}
 
 </script>
 
