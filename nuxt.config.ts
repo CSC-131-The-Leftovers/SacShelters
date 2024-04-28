56eb6f4a6887abcbc44ca0c0a4c5c2107aa84ce8
Carter Lavering <carterlavering@gmail.com>
2024-04-27 20:55:17 -0700

    supabase auth moved to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
   supabase: {
+    url: "https://wdvgskhdizkqfhzwpfdf.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indkdmdza2hkaXprcWZoendwZmRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5MTQ1NjYsImV4cCI6MjAyOTQ5MDU2Nn0.QYXVmflyw93ysJv8duxgy10RIHjGdF8jd0txyv1WbD4",
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
