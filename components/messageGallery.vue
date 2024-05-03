d00f956b53dbf05d63150a00ce436b4477cf64ae
Phillip_Nguyen <a2phillipnguyen@gmail.com>
2024-05-02 21:51:12 -0700

    trying to display messages but failing

---
+++ components/messageGallery.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="overflow-hidden text-gray-700">
+    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+      <div class="-m-1 flex flex-wrap md:-m-2">
+        <message
+          v-for="message in message"
+          :phone_num="message.phone_number"
+          :dateTime="message.dateTime"
+          :message="message.message"
+          :key="message.id" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  message: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
