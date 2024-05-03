cceae295d87eea18a032bead2ee096c8ea873d17
Carter Lavering <carterlavering@gmail.com>
2024-05-02 18:35:57 -0700

    initial server route

--- components/ResourceList.vue
+++ components/ResourceList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="flex h-screen w-64 flex-col gap-2 bg-base-200 p-2">
+      <p>{{ googleMaps }}</p>
       <NuxtLink
         v-for="r in resources"
         :key="r.id"
@@ -16,7 +17,7 @@
 <script setup lang="js">
 const supabase = useSupabaseClient();
 
-const { data } = await useAsyncData("resources", async () => {
+const { data: resources } = await useAsyncData("resources", async () => {
   const { data } = await supabase
     .from("resource")
     .select()
@@ -24,7 +25,11 @@ const { data } = await useAsyncData("resources", async () => {
   return data;
 });
 
-const resources = ref(data);
+const { data: googleMaps } = await useFetch("/api/googleMaps");
+
+console.log(googleMaps);
+
+// const resources = ref(data);
 </script>
 
 <style scoped>
