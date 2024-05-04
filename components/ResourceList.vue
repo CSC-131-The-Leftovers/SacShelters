c684c30eb924abed6a295e934407276ef3ea7de1
Carter Lavering <carterlavering@gmail.com>
2024-05-03 23:58:57 -0700

    sidebar now works with google maps

--- components/ResourceList.vue
+++ components/ResourceList.vue
@@ -1,31 +1,58 @@
 <template>
   <div>
-    <div class="flex h-screen w-64 flex-col gap-2 bg-base-200 p-2">
-      <p>{{ googleMaps }}</p>
+    <div class="h-screen w-64 flex-col gap-2 bg-base-200">
+      <div class="bg-primary p-2">
+        <input
+          v-model="zip"
+          type="text"
+          class="input input-primary"
+          placeholder="zip code"
+          @keyup.enter="getMapsData" />
+      </div>
       <NuxtLink
+        v-for="r in mapsData"
+        :key="r['place_id']"
+        :to="`/shelters/${r['place_id']}`"
+        class="card bg-neutral p-2 text-neutral-content">
+        <p class="text-2xl font-bold">{{ r["name"] }}</p>
+        {{ r }}</NuxtLink
+      >
+      <!-- <NuxtLink
         v-for="r in resources"
         :key="r.id"
         :to="`/shelters/${r.id}`"
         class="card h-24 bg-neutral p-4 text-neutral-content">
         <h3 class="text-2xl font-bold">{{ r.name }}</h3>
         <p>{{ r.resource_type }}</p>
-      </NuxtLink>
+      </NuxtLink> -->
     </div>
   </div>
 </template>
 
 <script setup lang="js">
-const supabase = useSupabaseClient();
+// const supabase = useSupabaseClient();
 
-const { data: resources } = await useAsyncData("resources", async () => {
-  const { data } = await supabase
-    .from("resource")
-    .select()
-    .eq("resource_type", "Shelter");
-  return data;
-});
+// const { data: resources } = await useAsyncData("resources", async () => {
+//   const { data } = await supabase
+//     .from("resource")
+//     .select()
+//     .eq("resource_type", "Shelter");
+//   return data;
+// });
 
-const { data: googleMaps } = await useFetch("/api/googleMaps?zip=95628");
+const zip = useState("zip");
+
+const mapsData = useState("mapsData");
+
+async function getMapsData() {
+  // alert(`zip is ${zip.value}`);
+  const { data } = await await useFetch("/api/googleMaps", {
+    query: {
+      query: `homeless shelters near ${zip.value}`,
+    },
+  });
+  mapsData.value = data;
+}
 
 // const resources = ref(data);
 </script>
--- server/api/googleMaps.ts
+++ server/api/googleMaps.ts
@@ -2,36 +2,20 @@
 
 export default defineEventHandler(async (event) => {
   // get zip code from query
-  const { zip } = getQuery(event);
+  const { query } = getQuery(event);
 
-  // test api call
-  // console.log("Api call happening now");
-  // const testData = await $fetch("https://fakestoreapi.com/products", {
-  //   query: {
-  //     limit: 2,
-  //   },
-  // });
-  // console.log("Test data:", testData);
+  console.log(`doing a search for "${query}"`);
 
-  // api call
   const mapsData = await $fetch(
     "https://maps.googleapis.com/maps/api/place/textsearch/json",
     {
       query: {
-        query: "homeless shelters near 95628",
+        query: query,
         radius: "50000",
         key: "AIzaSyAdQXFzjMjL1277HwWNSBJxONHB97cc7JE",
       },
     },
   );
 
-  // api call
-  // const { mapsData } = await $fetch(
-  //   "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-  // );
-
-  return {
-    // message: `Hello this is definitely the google maps api. the zip you asked for is ${zip}`,
-    mapsData,
-  };
+  return mapsData["results"];
 });
